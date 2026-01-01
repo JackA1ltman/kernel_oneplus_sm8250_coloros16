f36459e1b0b77a1f926cd3ab18e2e476f96612c5
ermyltsor <lamiimuk@163.com>
2026-01-01 23:20:47 +0800

    treewide: Drop oplus phoenix

--- init/main.c
+++ init/main.c
@@ -102,10 +102,6 @@
 #define CREATE_TRACE_POINTS
 #include <trace/events/initcall.h>
 
-#ifdef OPLUS_FEATURE_PHOENIX
-#include "../drivers/soc/oplus/system/oplus_phoenix/oplus_phoenix.h"
-#endif  //OPLUS_FEATURE_PHOENIX
-
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -612,11 +608,6 @@ asmlinkage __visible void __init start_kernel(void)
 	trap_init();
 	mm_init();
 
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_MM_INIT_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
-
 	ftrace_init();
 
 	/* trace_printk can be enabled here */
@@ -688,10 +679,6 @@ asmlinkage __visible void __init start_kernel(void)
 
 	early_boot_irqs_disabled = false;
 	local_irq_enable();
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_LOCAL_IRQ_ENABLE);
-#endif //OPLUS_FEATURE_PHOENIX
 
 	kmem_cache_init_late();
 
@@ -760,11 +747,6 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
-
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DELAYACCT_INIT_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
 
 	acpi_subsystem_init();
 	arch_post_acpi_subsys_init();
@@ -1014,18 +996,10 @@ static void __init do_basic_setup(void)
 	cpuset_init_smp();
 	shmem_init();
 	driver_init();
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DRIVER_INIT_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
 	init_irq_proc();
 	do_ctors();
 	usermodehelper_enable();
 	do_initcalls();
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DO_INITCALLS_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
 }
 
 static void __init do_pre_smp_initcalls(void)
@@ -1107,10 +1081,6 @@ static inline void mark_readonly(void)
 }
 #endif
 
-#if IS_BUILTIN(CONFIG_OPLUS_FEATURE_PHOENIX_REBOOT_SPEED)
-extern void bootprof_log_boot(char *str);
-#endif
-
 static int __ref kernel_init(void *unused)
 {
 	int ret;
@@ -1133,13 +1103,6 @@ static int __ref kernel_init(void *unused)
 
 	rcu_end_inkernel_boot();
 
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_INIT_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
-#if IS_BUILTIN(CONFIG_OPLUS_FEATURE_PHOENIX_REBOOT_SPEED)
-	bootprof_log_boot("Kernel_init_done");
-#endif
 	if (ramdisk_execute_command) {
 		ret = run_init_process(ramdisk_execute_command);
 		if (!ret)
@@ -1206,10 +1169,6 @@ static noinline void __init kernel_init_freeable(void)
 
 	do_basic_setup();
 
-#ifdef OPLUS_FEATURE_PHOENIX
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DO_BASIC_SETUP_DONE);
-#endif //OPLUS_FEATURE_PHOENIX
 	/* Open the /dev/console on the rootfs, this should never fail */
 	if (ksys_open((const char __user *) "/dev/console", O_RDWR, 0) < 0)
 		pr_err("Warning: unable to open an initial console.\n");
